19d9202063491d932a60a2a0bb461798e82f94bc
Charfeddine Hamrouni <chamrouni@mac-chamrouni.local>
2023-11-28 23:02:25 +0100

    init

---
+++ lib/components/woosmap.vue
@@ -0,0 +1,32 @@
+<template>
+  <div ref="woosmapContainer" class="woosmap-container" />
+</template>
+
+<script>
+export default {
+  props: {
+    options: {
+      type: Object,
+      default: () => ({})
+    }
+  },
+  mounted () {
+    this.$nextTick(() => {
+      this.initWoosmap()
+    })
+  },
+  methods: {
+    async initWoosmap () {
+      await this.$loadWoosmapSDK()
+      this.map = new woosmap.map.Map(this.$refs.woosmapContainer, this.options)
+    }
+  }
+}
+</script>
+
+<style>
+.woosmap-container {
+  width: 100%;
+  height: 400px; /* Adjust as needed */
+}
+</style>
